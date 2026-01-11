2fdbc8edc0e5399f66a96b020a3176199e1ab6ac
Seoyoung Park <seokachuu@gmail.com>
2026-01-12 05:56:51 +0900

    refactor: WeatherCard 이동 및 위젯화

---
+++ src/widgets/weather/index.ts
@@ -0,0 +1,3 @@
+export { default as WeatherCard } from './ui/WeatherCard';
+export { default as HourlyWeather } from './ui/HourlyWeather';
+export { default as WeatherSummary } from './ui/WeatherSummary';
